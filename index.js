4d269bcef5c37e00cf5b38716aea247c1a75dd90
barisusakli <barisusakli@gmail.com>
2015-08-13 16:19:20 -0400

    ip

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var db = module.parent.require('./database'),
 	plugin.init = function(params, callback) {
 
 		params.router.get('/api/saas/pageviews', function(req, res, next) {
-			if (req.ip !== '10.0.3.1') {
+			if (req.ip !== '10.0.3.1' && req.ip !== '::ffff:10.0.3.1') {
 				return res.json(403, 'not-allowed');
 			}
 			analytics.getMonthlyPageViews(function(err, stats) {
