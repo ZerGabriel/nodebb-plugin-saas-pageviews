ea1bf092ceca650a7000d20020afbe73a9db1fad
barisusakli <barisusakli@gmail.com>
2015-08-13 16:17:35 -0400

    added return

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var db = module.parent.require('./database'),
 
 		params.router.get('/api/saas/pageviews', function(req, res, next) {
 			if (req.ip !== '10.0.3.1') {
-				res.json(403, 'not-allowed');
+				return res.json(403, 'not-allowed');
 			}
 			analytics.getMonthlyPageViews(function(err, stats) {
 				if (err) {
